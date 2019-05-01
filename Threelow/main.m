0199e2e9e83e13b879732d3798c0315389664836
Dayson Dong <Daysond@outlook.com>
2019-05-01 10:36:39 -0400

    Initial Commit

---
+++ Threelow/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Threelow
+//
+//  Created by Dayson Dong on 2019-05-01.
+//  Copyright © 2019 Dayson Dong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
